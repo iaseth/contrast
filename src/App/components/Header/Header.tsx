aff63008c7d624300eeae87ea8a3229187adce74
Ankur Seth <iankurseth@gmail.com>
2024-09-01 20:47:29 +0530

    Now doing with only 2 HeaderButtons

--- src/App/components/Header/Header.tsx
+++ src/App/components/Header/Header.tsx
@@ -12,11 +12,13 @@ export default function Header ({
 }: HeaderProps) {
 	return (
 		<header className="absolute top-12 left-1/2 -translate-x-1/2 w-full max-w-xl px-4" onClick={e => e.stopPropagation()}>
-			<section className="bg-zinc-100 grid grid-cols-4 gap-0.5 text-center">
-				<HeaderButton text1="Background" text2="Color" hexColor={bgHexColor} />
-				<HeaderButton text1="Background" text2="Shade" hexColor={bgHexColor} />
-				<HeaderButton text1="Text" text2="Color" hexColor={textHexColor} />
-				<HeaderButton text1="Text" text2="Shade" hexColor={textHexColor} />
+			<section className="">
+				<header className="bg-zinc-100 grid grid-cols-2 gap-1 text-center">
+					<HeaderButton text="Background" hexColor={bgHexColor} />
+					<HeaderButton text="Text" hexColor={textHexColor} />
+				</header>
+
+				<footer></footer>
 			</section>
 		</header>
 	);
--- src/App/components/Header/HeaderButton/HeaderButton.tsx
+++ src/App/components/Header/HeaderButton/HeaderButton.tsx
@@ -2,22 +2,20 @@
 
 
 interface HeaderButtonProps {
-	text1: string,
-	text2: string,
+	text: string,
 	hexColor: string
 }
 
 export default function HeaderButton ({
-	text1, text2, hexColor
+	text, hexColor
 }: HeaderButtonProps) {
 	return (
-		<button className="grid grid-cols-4 bg-zinc-600 text-zinc-100 border-2 border-zinc-600">
-			<section className="col-span-3 px-2 py-2 text-xs font-bold text-left">
-				<div>{text1}</div>
-				<div>{text2}</div>
+		<button className="grid grid-cols-4 bg-white text-zinc-800 border-2 border-zinc-600">
+			<section className="col-span-3 px-4 py-4 text-sm font-bold text-left">
+				<div>{text}</div>
 			</section>
 
-			<section className="h-full bg-red-500" style={{backgroundColor: hexColor}}></section>
+			<section className="border-l-2 border-zinc-600 h-full bg-red-500" style={{backgroundColor: hexColor}}></section>
 		</button>
 	);
 }
